47d0e8f33a3093c9bcf8f51c3792c2af6b81d496
nguyenhuy47 <huynguyen.uct47@gmail.com>
2019-10-17 10:16:37 +0700

    cap nhat giao dien

--- resources/views/songs/create.blade.php
+++ resources/views/songs/create.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!doctype html>
 <html lang="en">
 <head>
@@ -63,11 +62,11 @@
             </div>
             <div class="form-group">
                 <label>Ca sĩ</label>
-                <input type="text" class="form-control" name="singer_id" placeholder="Tên ca sĩ">
+                <input type="text" class="form-control" name="singer_name" placeholder="Tên ca sĩ">
             </div>
             <div class="form-group">
                 <label>Nhạc sĩ</label>
-                <input type="text" class="form-control" name="artist_id" placeholder="Nhạc sĩ">
+                <input type="text" class="form-control" name="artist_name" placeholder="Nhạc sĩ">
             </div>
             <div class="form-group">
                 <label>Thể loại</label>
