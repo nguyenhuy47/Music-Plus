e60de60aeaf9d6b389939d3d8f2c3690cdd7b11b
Duc <quangduc035@gmail.com>
2019-10-28 15:27:28 +0700

    profile update 3

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -17,7 +17,7 @@
 
             @if (count($errors) > 0)
                 <div class="alert alert-danger">
-                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                    <strong>Rất tiếc!</strong>Có một số vấn đề với đầu vào của bạn.<br><br>
                     <ul>
                         @foreach ($errors->all() as $error)
                             <li>{{ $error }}</li>
@@ -33,23 +33,22 @@
                     <img id="image" height="150" width="150" class="rounded-circle" src="{{asset('/storage/avatars/'.$user->avatar) }}" />
                     <!-- badge -->
                     <div class="rank-label-container">
-                        <span class="label label-default rank-label">{{$user->name}}</span>
                     </div>
                 </div>
             </div>
 
-        </div>
-        <div class="row justify-content-center">
+
             <form action="/profile" method="post" enctype="multipart/form-data">
                 @csrf
+                <span class="label label-default rank-label">{{$user->name}}</span>
                 <div class="form-group">
                     <input type="file" class="form-control-file" name="avatar" id="avatarFile" aria-describedby="fileHelp"
                            onchange="document.getElementById('image').src = window.URL.createObjectURL(this.files[0])"
                            class="form-control-file">
 
-                    <small id="fileHelp" class="form-text text-muted">Please upload a valid image file. Size of image should not be more than 2MB.</small>
+                    <small id="fileHelp" class="form-text text-muted">Vui lòng tải lên một tập tin hình ảnh hợp lệ. Kích thước của hình ảnh không được quá 2MB.</small>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Cập nhập</button>
             </form>
         </div>
     </div>
